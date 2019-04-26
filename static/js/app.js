008a9454fca86ecd07d970f316da5acac29611e1
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-04-26 11:38:47 -0500

    working code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
-var tableData = data;
+var tableData = "";
 
-// YOUR CODE HERE!
+// Select table body
+const tbody = d3.select("#ufo-table>tbody")
+const btn = d3.select("#filter-btn")
+let selected = "datetime"
+
+function createTable(inputArray){
+  // create row for each sighting
+  tbody.selectAll("tr").remove();
+  inputArray.forEach((sighting) => {
+    const row = tbody.append("tr");
+    for (key in sighting){
+      const cell = row.append("td");
+      cell.html(sighting[key])
+    }
+  }
+)
+}
+
+d3.select("select")
+  .on("change",function(){
+    selected = d3.select("#d3-dropdown").node().value;
+    d3.select("#datetime").node().value = ""
+})
+
+btn.on("click", function() {
+  d3.event.preventDefault()
+  const input_form = d3.select("#datetime")
+  // Get the value property of the input element
+  const targetValue = input_form.property("value")
+  if (targetValue !== ""){
+    filteredData = data.filter(sighting => sighting[selected] === targetValue)
+    // tbody.selectAll("tr").remove();
+    createTable(filteredData)
+  }
+  // if filter string is empty display all tableData
+  else {
+    createTable(data)
+  }
+  input_form.node().value = ""
+
+})
+
+createTable(data)
